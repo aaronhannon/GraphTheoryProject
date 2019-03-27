b7c96d3ab80f2674d44afe3f87dd2bc8baa67739
aaronhannon <aaronchannon98@gmail.com>
2019-03-27 10:02:07 +0000

    Shunt method finished and tested, comments yet to be added

--- Project.py
+++ Project.py
@@ -4,10 +4,25 @@
 
 def shunt(infix):
     specials = {'*': 50, '.': 40, '|':30}
-    pofix = ""
     stack = ""
+    postfix = ""
+    
 
-    #loop through infix string
+    for c in infix:
+        if c == '(':
+            stack = stack + c
+        elif c == ')':
+            while stack[-1] != '(':
+                postfix,stack = postfix + stack[-1],stack[:-1]
+            stack = stack[:-1]
+        elif c in specials:
+            while stack and specials.get(c,0) <= specials.get(stack[-1],0):
+                postfix,stack = postfix + stack[-1],stack[:-1]
+            stack = stack + c
+        else:
+            postfix = postfix + c
+    while stack:
+        postfix,stack = postfix + stack[-1],stack[:-1]
+    return postfix
 
-
-    return pofix
+print(shunt("a.(b|d).c*"))
