3130bb15d8db820ab89ca77c7d39ca2950abf6b2
aaronhannon <aaronchannon98@gmail.com>
2019-03-27 09:37:09 +0000

    Shunt method added

---
+++ Project.py
@@ -0,0 +1,13 @@
+#Graph Theory Project - G00347352 - Aaron Hannon
+
+#Shunting algorithm
+
+def shunt(infix):
+    specials = {'*': 50, '.': 40, '|':30}
+    pofix = ""
+    stack = ""
+
+    #loop through infix string
+
+
+    return pofix
